8560f176f738b6dd39d0da37b811d53981600d1e
maebusko <156959312+maebusko@users.noreply.github.com>
2025-11-17 14:47:43 -0500

    Update About.jsx

--- website/src/pages/About.jsx
+++ website/src/pages/About.jsx
@@ -18,6 +18,8 @@ export default function About() {
         <h2 className="text-2xl font-semibold mb-2">Our Problem:</h2>
           <p className="mb-2">
             The LAM Museum at Wake Forest University is currently is currently in possession of 400 clay human figurines that they would like to repatriate to Mexico. However, there is no known provenance for any of the figurines or any documentation of each artifact’s traits. The Lam Museum does not have any system in place to accurately identify or classify any of the artifacts. Manual classification is labor-intensive and requires specialized expertise and is further complicated by the subjective nature of traditional archaeological categorization, which lacks well-defined, quantitative criteria. Currently, they do not have the staff or time available to build its own system. If they were to make time to do this, other essential managing activities for the museum would have to be halted, causing major issues. Students and staff also cannot efficiently sort through the collection to determine where these figurines belong. Furthermore, analyzing the details of so many artifacts by hand would be time-consuming and may lead to mistakes, which would decrease public and educational engagement. A seasoned expert in the field is also required for identification, and it is neither cost-effective nor timely to acquire one. 
+          </p>
+        </div>
            
         ← Back to Home
       </Link>
