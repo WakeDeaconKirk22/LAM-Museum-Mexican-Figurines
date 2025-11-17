3b10919c744b21d038eac2a385b3c1046c25f2cb
acfatemi <156959703+acfatemi@users.noreply.github.com>
2025-11-17 14:42:53 -0500

    Update Home.jsx

--- website/src/pages/Home.jsx
+++ website/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import DigitCsvClassifier from "../DigitCsvClassifier";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center py-10">
-      <h1 className="text-3xl font-bold mb-4">Digit CSV Classifier</h1>
+      <h1 className="text-3xl font-bold mb-4">HOMEEEEEDigit CSV Classifier</h1>
       <DigitCsvClassifier />
     </div>
   );
